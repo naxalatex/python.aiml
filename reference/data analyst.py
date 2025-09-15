f99746afa21a83c6804699ecee49b10566c7b265
sidharthsivacochin@gmail.com <sidharthsivacochin@gmail.com>
2025-09-15 12:38:55 +0530

    minor updates

---
+++ reference/data analyst.py
@@ -0,0 +1,17 @@
+#DATA ANALYST
+#A process of inspecting, cleaning, transforming  data with the goal of discovering useful information, informing conclusion and supporting decision making
+# exploring knowledge and insight from raw data into business logic
+#looks backward(interprets and explains)
+# focuses on examining existing, often structered data
+#DATA SCIENCE
+##a broad, multidiscplinary field.
+# focuses on asking new,strategic questions
+#uses tools like SQL, Excel
+#look forward (predicts and innovates)
+#analyst : checks the past and shows the reason why its happend (earthquake)
+#data science : predicts if it would occure again in the future (earthquake)
+## A module in pythin is just a .py file that cinatains functions, classes or variable which you can import and reuse in another file
+#synatx -> import module name 
+import c
+print(c.add(5,6))
+print(c.subtract(9,0))
